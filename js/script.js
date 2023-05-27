6d20ba4cbc87dc43cab44b868b176e9231adbeed
Sebastián Ucedo <sebasucedo@gmail.com>
2023-05-26 23:40:20 -0300

    Basic info from json

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 window.onbeforeprint = function() {
-  var element = document.getElementById("myElement");
+  var element = document.getElementById("basicInfo");
   element.classList.remove("is-three-quarters");
   element.classList.add("is-full");
 };
 
 window.onafterprint = function() {
-  var element = document.getElementById("myElement");
+  var element = document.getElementById("basicInfo");
   element.classList.remove("is-full");
   element.classList.add("is-three-quarters");
 };
